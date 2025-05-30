6f3a27ae73566567451b0388a1aaa5bca2c696ab
Kshitij Sahdev <kshitijsahdev5@gmail.com>
2025-05-30 22:50:34 +0530

    Update variables.tf

--- TASK 1/variables.tf
+++ TASK 1/variables.tf
@@ -1,5 +1,5 @@
-# variables.tf file contains variables for the Terraform configuration.
-# terraform.tfvars contains the values for the variables defined in this file.
+# variables.tf got variables for Terraform configuration
+# terraform.tfvars got values for variables defined here
 
 variable "ami_id" {
   type        = string
